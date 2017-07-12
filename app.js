8ed67705b7aa25f3bdb3363cadbb1512f5f81c8c
Gabe Altenberger <gabealtenberger@gmail.com>
2017-07-11 17:24:25 -0700

    About Me finished

---
+++ app.js
@@ -0,0 +1,51 @@
+'use strict'
+// qurestion1
+var answer1 = prompt('Hello! Is my name Gabe?').toUpperCase();
+if (answer1 === "YES" || answer1 === "Y") {
+  alert('That is correct my name is Gabe!');
+  console.log('The user answered ' + answer1 + ' to whether my name was Gabe.')
+}
+else {
+  alert('Sorry that is incorrect');
+  console.log('The user answered ' + answer1 + ' to whether my name was Gabe.')
+}
+// question2
+var answer2 = prompt('Am I 18 years old?').toUpperCase();
+if (answer2 === "NO" || answer2 === "N") {
+  alert('That is correct I am not 18. I am actually 19 years old!');
+  console.log('the user answered ' + answer2 + ' to whether I am 18 years old.')
+}
+else {
+  alert('Sorry that is incorrect');
+  console.log('The user answered ' + answer2 + ' to whether I am 18 years old.')
+}
+// question3
+var answer3 = prompt('Am I from Brier, Washington?').toUpperCase();
+if (answer3 === "YES" || answer3 === "Y") {
+  alert('That is correct I am from Brier, Washington!');
+  console.log('the user answered ' + answer3 + ' to whether I am from Brier, Washington.')
+}
+else {
+  alert('Sorry that is incorrect');
+  console.log('The user answered ' + answer3 + ' to whether I am 18 years old.')
+}
+// question4
+var answer2 = prompt('Are my first two hobbies listed, that I love to shoot hoops and play my guitar?').toUpperCase();
+if (answer2 === "YES" || answer2 === "Y") {
+  alert('That is correct! I do love to shoot hoops, as well as, play my guitar!');
+  console.log('The user answered ' + answer2 + ' to whether my first two hobbies were that I love to shoot hoops and play my guitar.')
+}
+else {
+  alert('Sorry that is incorrect');
+  console.log('The user answered ' + answer2 + ' to whether my first two hobbies were that I love to shoot hoops and play my guitar.')
+}
+// question5
+var answer2 = prompt('Is my last hobby listed, that I love to go fishing?').toUpperCase();
+if (answer2 === "NO" || answer2 === "N") {
+  alert('That is correct! I actually prefer hanging out with friends, rather than going fishing');
+  console.log('The user answered ' + answer2 + ' to whether my last hobby was that I love to go fishing.')
+}
+else {
+  alert('Sorry that is incorrect');
+  console.log('The user answered ' + answer2 + ' to whether my last hobby was that I love to go fishing.')
+}
