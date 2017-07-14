2bd0ed7eb1c28bfd7486e9aca8bc23c2bed67e1d
Gabe Altenberger <gabealtenberger@gmail.com>
2017-07-13 17:35:00 -0700

    Fixed question 7

--- app.js
+++ app.js
@@ -1,54 +1,33 @@
-'use strict'
-// question1
-var answer1 = prompt('Hello! Is my name Gabe?').toUpperCase();
-if (answer1 === "YES" || answer1 === "Y") {
-  alert('That is correct my name is Gabe!');
-  console.log('The user answered ' + answer1 + ' to whether my name was Gabe.')
+'use strict';
+
+// function that asks each question
+function question(i) {
+
+  var ans = prompt(questionArray[i]).toUpperCase();
+
+  if(ans === correctAnswer[i]) {
+    alert(correctAlert[i]);
+    console.log('got it right')
+  } else {
+    alert(wrongAlert[i]);
+    console.log('got it wrong');
+  }
+
 }
-else {
-  alert('Sorry that is incorrect');
-  console.log('The user answered ' + answer1 + ' to whether my name was Gabe.')
-}
-// question2
-var answer2 = prompt('Am I 18 years old?').toUpperCase();
-if (answer2 === "NO" || answer2 === "N") {
-  alert('That is correct I am not 18. I am actually 19 years old!');
-  console.log('the user answered ' + answer2 + ' to whether I am 18 years old.')
-}
-else {
-  alert('Sorry that is incorrect');
-  console.log('The user answered ' + answer2 + ' to whether I am 18 years old.')
-}
-// question3
-var answer3 = prompt('Am I from Brier, Washington?').toUpperCase();
-if (answer3 === "YES" || answer3 === "Y") {
-  alert('That is correct I am from Brier, Washington!');
-  console.log('the user answered ' + answer3 + ' to whether I am from Brier, Washington.')
-}
-else {
-  alert('Sorry that is incorrect');
-  console.log('The user answered ' + answer3 + ' to whether I am 18 years old.')
-}
-// question4
-var answer2 = prompt('Are my first two hobbies listed, that I love to shoot hoops and play my guitar?').toUpperCase();
-if (answer2 === "YES" || answer2 === "Y") {
-  alert('That is correct! I do love to shoot hoops, as well as, play my guitar!');
-  console.log('The user answered ' + answer2 + ' to whether my first two hobbies were that I love to shoot hoops and play my guitar.')
-}
-else {
-  alert('Sorry that is incorrect');
-  console.log('The user answered ' + answer2 + ' to whether my first two hobbies were that I love to shoot hoops and play my guitar.')
-}
-// question5
-var answer2 = prompt('Is my last hobby listed, that I love to go fishing?').toUpperCase();
-if (answer2 === "NO" || answer2 === "N") {
-  alert('That is correct! I actually prefer hanging out with friends, rather than going fishing');
-  console.log('The user answered ' + answer2 + ' to whether my last hobby was that I love to go fishing.')
-}
-else {
-  alert('Sorry that is incorrect');
-  console.log('The user answered ' + answer2 + ' to whether my last hobby was that I love to go fishing.')
-}
+
+// variables to input into each iteration of the function
+var questionArray = ['Hello! Is my name Gabe?', 'Am I 18 years old?', 'Am I from Brier, Washington?', 'Are my first two hobbies listed, that I love to shoot hoops and play my guitar?', 'Is my last hobby listed, that I love to go fishing?'];
+var correctAnswer = ['YES', 'NO', 'YES', 'YES', 'NO'];
+var correctAlert = ['That is correct my name is Gabe!', 'That is correct I am not 18. I am actually 19 years old!', 'That is correct I am from Brier, Washington!', 'That is correct! I do love to shoot hoops, as well as, play my guitar!', 'That is correct! I actually prefer hanging out with friends, rather than going fishing'];
+var wrongAlert = ['Sorry that is incorrect', 'Sorry that is incorrect', 'Sorry that is incorrect', 'Sorry that is incorrect', 'Sorry that is incorrect']
+
+// calling function for each question
+question(0);
+question(1);
+question(2);
+question(3);
+question(4);
+
 // question6
 
 var numGuesses = 0;
